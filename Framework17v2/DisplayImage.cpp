768a0762e70fa553cfa8bae6d08966cdb4734fb3
kusmierzrafal <rafal.kusmierz.biz@gmail.com>
2026-03-10 20:35:38 +0100

    first_images_display

---
+++ Framework17v2/DisplayImage.cpp
@@ -0,0 +1,19 @@
+﻿#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Start ..." << std::endl;
+
+    std::string image_name = "lidl_logo_1.png";
+
+    cv::Mat image = cv::imread(image_name);
+
+    cv::imshow(image_name, image);
+
+
+    cv::waitKey(-1);
+    return 0;
+}
